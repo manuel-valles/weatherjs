8825c07961e5b2942c6d76e46d728ab68b0cbf7b
manukempo <manukempo@gmail.com>
2018-03-11 13:35:43 +0000

    Display the weather - class UI

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
+// Init UI object
+const ui = new UI();
+
 // weather.changeLocation('Miami', 'FL');
 
 // Get weather on DOM Load
@@ -8,6 +11,6 @@ document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather(){
   weather.getWeather()
-    .then(results => console.log(results))
+    .then(results => ui.paint(results))
     .catch(err => console.log(err));
 }
